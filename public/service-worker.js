82e1ad43a730ccf69a30da79371c3688e543c95e
imAnonAmi <mcintireaw@gmail.com>
2020-05-21 14:50:12 -0700

    I think I modified all the correct files to make this work, but it is not functioning offline.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
-const FILES_TO_CACHE = ["/", "/index.html", "app.js", "favicon.ico"];
+const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/db.js", "/styles.css" ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v42";
+const DATA_CACHE_NAME = "data-cache-v666";
 
 // install
 self.addEventListener("install", function(evt) {
